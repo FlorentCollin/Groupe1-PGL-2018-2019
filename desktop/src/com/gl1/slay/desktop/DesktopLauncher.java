eb04488f0dcd045dce9b9a91a06389bee0d885af
FlorentCollin <florent.collin23@gmail.com>
2019-01-27 20:52:17 +0100

    Ajout des classes nécessaire à l'utilisation de LibGDX

---
+++ desktop/src/com/gl1/slay/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gl1.slay.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gl1.slay.Slay;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Slay(), config);
+	}
+}
