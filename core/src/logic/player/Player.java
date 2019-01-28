7c6c116311e89b6733960af39db9707707b3d1d5
JustinCassart <justin.cassart.4567@gmail.com>
2019-01-28 02:39:16 +0100

    Modification de la classe Player

--- core/src/logic/player/Player.java
+++ core/src/logic/player/Player.java
@@ -1,11 +1,44 @@
-package player;
+package logic.player;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.graphics.Color;
+
+import logic.item.Soldier;
 
 public class Player {
 	private String name;
 	private Color color;
-	private ArrayList<Soldier> soldiers;
+	protected ArrayList<Soldier> soldiers;
 	
 	public Player() {
-		
+		/*Dans le cas où le joueur ne stipule rien ou est une ia*/
+		soldiers =  new ArrayList<Soldier>();
+	}
+	
+	public Player(String name, Color color) {
+		/*Dans le cas d'un véritable joueur modifiant des paramètres*/
+		this.name = name;
+		this.color = color;
+		soldiers = new ArrayList<Soldier>();
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
+	
+	public void setColor(Color color) {
+		// n'a une utilité que si il est possible
+		// de changer se couleur pendant la partie
+		this.color = color;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		//utilité idem que pour setColor
+		this.name = name;
 	}
 }
