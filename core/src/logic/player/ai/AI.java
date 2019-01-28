9db51443477dbabf824b6c3cca8f4584735fbd7c
JustinCassart <justin.cassart.4567@gmail.com>
2019-01-28 02:42:22 +0100

    Modification du contructeur et ajout de la méthode play

--- core/src/logic/player/ai/AI.java
+++ core/src/logic/player/ai/AI.java
@@ -1,23 +1,26 @@
 package logic.player.ai;
 
+import java.util.ArrayList;
+
 import logic.board.Board;
 import logic.board.cell.Cell;
-import logic.item.Soldier;
+import logic.player.Player;
 
-public class AI {
+public class AI extends Player{
 	private Strategy strategy;
 	private Board board;
-	private Cell ownCells[];
-	private Soldier selectedSoldier;
+	private ArrayList<Cell> ownCells;
 	
-	public AI(Strategy strategy) {
+	public AI(Strategy strategy, Board board) {
 		this.strategy = strategy;
+		this.board = board;
+		ownCells = new ArrayList<Cell>();
 	}
 	
 	/* 
 	 * Permet à l'ia de jouer son tour en fonction de la stratégie qu'elle utilise
 	 * */
 	public void play() {
-		/* TO DO */
+		this.strategy.play(board, ownCells, soldiers);
 	}
 }
