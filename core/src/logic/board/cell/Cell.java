b4305455375a953104cfaeb80077b0023e4e2640
JustinCassart <justin.cassart.4567@gmail.com>
2019-02-04 14:06:59 +0100

    Ajout constructeur

--- core/src/logic/board/cell/Cell.java
+++ core/src/logic/board/cell/Cell.java
@@ -6,16 +6,34 @@ import logic.item.Item;
 public class Cell {
 	private Item item;
 	private District district;
-	private int numberOfAdjacentWaterCell;
-	private boolean actifIncome;
+	private int numberOfAdjacentWaterCell; //?
+	private boolean actifIncome; //?
 	protected boolean accessible;
 
 	public Cell() {
 		
 	}
 	
+	public Cell(District district) {
+		this.district = district;
+		// Pour n'appeler qu'une seule méthode
+		district.addCell(this);
+	}
+	
 	public void setItem(Item item) {
 		this.item = item;
+	}
+	
+	public Item getItem() {
+		return this.item;
+	}
+	
+	public void setDistrcit(District district) {
+		this.district = district;
+	}
+	
+	public District getDistrict() {
+		return this.district;
 	}
 	
 }
