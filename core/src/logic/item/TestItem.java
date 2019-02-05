8891fa726697004d785f487215094d11f446523e
JustinCassart <justin.cassart.4567@gmail.com>
2019-02-05 21:51:32 +0100

    Modif du test

--- core/src/logic/item/TestItem.java
+++ core/src/logic/item/TestItem.java
@@ -5,6 +5,20 @@ import logic.item.level.TowerLevel;
 
 public class TestItem {
 	public static void main(String[] args) {
-		
+		int rows = 2;
+		int columns = 3;
+		String[][] board= new String[rows][columns];
+		System.out.println(board[0][0]);
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				board[i][j] = "(i:"+String.valueOf(i)+" j:"+String.valueOf(j)+")";
+			}
+		}
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j<columns; j++) {
+				System.out.print(board[i][j] +" ");
+			}
+			System.out.println();
+		}
 	}
 }
