ecb57ff9b0034fb24a0580805ac3052793d27221
JustinCassart <justin.cassart.4567@gmail.com>
2019-02-05 18:27:36 +0100

    Modification du test

--- core/src/logic/item/TestItem.java
+++ core/src/logic/item/TestItem.java
@@ -1,11 +1,10 @@
 package logic.item;
 
+import logic.item.level.SoldierLevel;
+import logic.item.level.TowerLevel;
+
 public class TestItem {
 	public static void main(String[] args) {
-		Soldier soldier = new Soldier(null);
-		System.out.println("Buyable : "+soldier.getMode().isBuyable()); 
-		System.out.println("Improvable : "+soldier.getMode().isImprovable());
-		System.out.println("Movable : "+soldier.getMode().isMovable());
-	
+		
 	}
 }
