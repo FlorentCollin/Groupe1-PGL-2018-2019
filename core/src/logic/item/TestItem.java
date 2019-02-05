dd1ef1f404115454bdd6d2cec37f5a257b09e21b
JustinCassart <justin.cassart.4567@gmail.com>
2019-02-05 23:19:19 +0100

    Update TestItem.java

--- core/src/logic/item/TestItem.java
+++ core/src/logic/item/TestItem.java
@@ -1,24 +1,23 @@
 package logic.item;
 
-import logic.item.level.SoldierLevel;
-import logic.item.level.TowerLevel;
+import java.util.ArrayList;
 
 public class TestItem {
 	public static void main(String[] args) {
-		int rows = 2;
-		int columns = 3;
-		String[][] board= new String[rows][columns];
-		System.out.println(board[0][0]);
-		for(int i = 0; i < rows; i++) {
-			for(int j = 0; j < columns; j++) {
-				board[i][j] = "(i:"+String.valueOf(i)+" j:"+String.valueOf(j)+")";
-			}
+		ArrayList<Integer> l1 = new ArrayList<Integer>();
+		ArrayList<Integer> l2 = new ArrayList<Integer>();
+		for(int i = 0; i<5; i++) {
+			l1.add(i);
+			l2.add(5-i);
 		}
-		for(int i = 0; i < rows; i++) {
-			for(int j = 0; j<columns; j++) {
-				System.out.print(board[i][j] +" ");
-			}
-			System.out.println();
-		}
+		System.out.println(l1.equals(l2));
+		System.out.println(l1.indexOf(2));
+		System.out.println(l1.indexOf(44));
+		int[] m1 = new int[2];
+		int[] m2 = new int[2];
+		m1[0] = 0;
+		m1[1] = 1;
+		m2[0] = 0;
+		m2[1] = 1;
 	}
 }
