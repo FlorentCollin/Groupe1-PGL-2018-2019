d1c464e791682d10038ee775be56a158af2b228a
JustinCassart <justin.cassart.4567@gmail.com>
2019-02-07 13:22:48 +0100

    Update TestItem.java

--- core/src/logic/item/TestItem.java
+++ core/src/logic/item/TestItem.java
@@ -1,23 +1,13 @@
 package logic.item;
 
-import java.util.ArrayList;
+import logic.item.level.SoldierLevel;
+import logic.player.Player;
 
 public class TestItem {
 	public static void main(String[] args) {
-		ArrayList<Integer> l1 = new ArrayList<Integer>();
-		ArrayList<Integer> l2 = new ArrayList<Integer>();
-		for(int i = 0; i<5; i++) {
-			l1.add(i);
-			l2.add(5-i);
-		}
-		System.out.println(l1.equals(l2));
-		System.out.println(l1.indexOf(2));
-		System.out.println(l1.indexOf(44));
-		int[] m1 = new int[2];
-		int[] m2 = new int[2];
-		m1[0] = 0;
-		m1[1] = 1;
-		m2[0] = 0;
-		m2[1] = 1;
+		Player p = new Player();
+		Soldier s1 = new Soldier(p);
+		Soldier s2 = new Soldier(p, SoldierLevel.level4);
+		System.out.println(s1.getLevel().compareTo(s2.getLevel()));
 	}
 }
