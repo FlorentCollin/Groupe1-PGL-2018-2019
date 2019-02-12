a0d42d3385beb50b45c0e33a9c5544d331f97f3a
JustinCassart <justin.cassart.4567@gmail.com>
2019-02-12 15:41:58 +0100

    Test modifié

--- core/src/unitTests/CellTest.java
+++ core/src/unitTests/CellTest.java
@@ -1,5 +1,16 @@
 package unitTests;
 
-public class CellTest {
+import logic.item.Soldier;
+import logic.item.level.SoldierLevel;
+import logic.player.Player;
 
+public class CellTest {
+	public static void main(String[] args) {
+		Player p = new Player();
+		Soldier s = new Soldier(p, SoldierLevel.level2);
+		for(SoldierLevel l : SoldierLevel.values()) {
+			System.out.println(l.hashCode());
+		}
+		System.out.println(s.getLevel().hashCode());
+	}
 }
