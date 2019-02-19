f265061c3c416418ba4024c506ab83d2799de711
FlorentCo <florent.collin23@gmail.com>
2019-02-19 21:08:17 +0100

    Changement des couleurs des joueurs ainsi que des WaterCells

--- core/assets/worlds/hex.tsx
+++ core/assets/worlds/hex.tsx
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="hex" tilewidth="256" tileheight="222" tilecount="6" columns="0">
+<tileset version="1.2" tiledversion="1.2.2" name="hex" tilewidth="256" tileheight="222" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
    <property name="available" type="bool" value="true"/>
    <property name="player" type="int" value="1"/>
   </properties>
-  <image width="256" height="222" source="hex_blue.png"/>
+  <image width="256" height="222" source="hex_green.png"/>
  </tile>
  <tile id="1">
   <properties>
@@ -27,7 +27,7 @@
    <property name="available" type="bool" value="true"/>
    <property name="player" type="int" value="1"/>
   </properties>
-  <image width="256" height="222" source="hex_blue_selected.png"/>
+  <image width="256" height="222" source="hex_green.png"/>
  </tile>
  <tile id="4">
   <properties>
@@ -42,5 +42,15 @@
    <property name="player" type="int" value="0"/>
   </properties>
   <image width="256" height="222" source="hex_neutral_selectedpng.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="available" type="bool" value="false"/>
+   <property name="player" type="int" value="0"/>
+  </properties>
+  <image width="256" height="222" source="hex_blue.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="222" source="hex_blue_selected.png"/>
  </tile>
 </tileset>
