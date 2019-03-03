b1450e50654add4d4ddfdf29aaf84fa2d0c064a6
Florent <florent.collin23@gmail.com>
2019-03-03 10:40:18 +0100

    Add HUD

---
+++ core/src/gui/Hud.java
@@ -0,0 +1,23 @@
+package gui;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Classe représentant l'HUD in game qui contient notamment le shop ainsi que le nom du joueur actif.
+ */
+public class Hud extends Stage {
+    /**
+     * Classe interne qui correspond au Shop (où il est possible d'acheter des soldats)
+     */
+   private class Shop {
+       public Shop() {
+           
+       }
+
+       public void addActorsToStage(Stage stage) {
+           //TODO
+       }
+
+    }
+}
