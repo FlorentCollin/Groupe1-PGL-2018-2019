a31b138f2bf4db3d88c384cd6928bb0190df8554
JustinCassart <justin.cassart.4567@gmail.com>
2019-03-10 10:50:58 +0100

    Update AI.java

--- core/src/logic/player/ai/AI.java
+++ core/src/logic/player/ai/AI.java
@@ -17,7 +17,7 @@ public class AI extends Player{
 		districts = new ArrayList<>();
 	}
 	
-	/* 
+	/**
 	 * Permet à l'ia de jouer son tour en fonction de la stratégie qu'elle utilise
 	 * */
 	public void play() {
