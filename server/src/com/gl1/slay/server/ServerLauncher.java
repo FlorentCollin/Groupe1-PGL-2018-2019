9bd9309b5cda7fae09f55ab0a4352b741645f02f
FlorentCo <florent.collin23@gmail.com>
2019-03-12 17:39:23 +0100

    fix port denied linux

--- server/src/com/gl1/slay/server/ServerLauncher.java
+++ server/src/com/gl1/slay/server/ServerLauncher.java
@@ -17,7 +17,7 @@ public class ServerLauncher {
     public static void main(String[] args) throws IOException {
         System.out.println("Starting server...");
         LinkedBlockingQueue<Message> messageToSend = new LinkedBlockingQueue<>();
-        ServerListener serverListener = new ServerListener(PORT, messageToSend);
+        ServerListener serverListener = new ServerListener(8888, messageToSend);
         ServerSender serverSender = new ServerSender(serverListener.getSelector(), messageToSend);
         //Démarrage des Threads
         serverSender.start();
