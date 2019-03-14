c7c12f40bbd55a69ffacfb12f9acc12de8a16c06
JustinCassart <justin.cassart.4567@gmail.com>
2019-03-14 13:30:09 +0100

    Modification

--- core/src/logic/naturalDisasters/NaturalDisasters.java
+++ core/src/logic/naturalDisasters/NaturalDisasters.java
@@ -1,9 +1,54 @@
 package logic.naturalDisasters;
 
-public class NaturalDisasters {
-	private int proba,
-				numberOfEffectedCells,
-				duration;
+import java.util.ArrayList;
+
+import logic.board.cell.Cell;
+
+public abstract class NaturalDisasters {
+	private int proba;
+	private int	numberOfEffectedCells;
+	private int duration;
 	
+	protected ArrayList<Cell> affectedCells;
+	
+	public NaturalDisasters() {
+		affectedCells = new ArrayList<>();
+	}
+
+	public int getProba() {
+		return proba;
+	}
+
+	public void setProba(int proba) {
+		this.proba = proba;
+	}
+
+	public int getNumberOfEffectedCells() {
+		return numberOfEffectedCells;
+	}
+
+	public void setNumberOfEffectedCells(int numberOfEffectedCells) {
+		this.numberOfEffectedCells = numberOfEffectedCells;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public ArrayList<Cell> getAffectedCells() {
+		return affectedCells;
+	}
+
+	public void setAffectedCells(ArrayList<Cell> affectedCells) {
+		this.affectedCells = affectedCells;
+	}
+	
+	public void play() {
+		
+	}
 	
 }
--- core/src/logic/naturalDisasters/NaturalDisastersController.java
+++ core/src/logic/naturalDisasters/NaturalDisastersController.java
@@ -2,14 +2,46 @@ package logic.naturalDisasters;
 
 import java.util.ArrayList;
 
+import logic.board.Board;
+
 public class NaturalDisastersController {
 	private ArrayList<NaturalDisasters> enableDisasters;
+	private ArrayList<NaturalDisasters> naturalDisasters;
+	private Blizzard blizzard;
+	private Drought drought;
+	private ForestFire forestFire;
+	private LandErosion landErosion;
+	private Tsunami tsunami;
+	private VolcanicEruption volcanicEruption;
 	
-	public NaturalDisastersController() {
-		this.enableDisasters = new ArrayList<NaturalDisasters>();
+	public NaturalDisastersController(Board board) {
+		enableDisasters = new ArrayList<>();
+		naturalDisasters = new ArrayList<>();
+		blizzard = new Blizzard();
+		naturalDisasters.add(blizzard);
+		drought = new Drought();
+		naturalDisasters.add(drought);
+		forestFire = new ForestFire();
+		naturalDisasters.add(forestFire);
+		landErosion = new LandErosion(board);
+		naturalDisasters.add(landErosion);
+		tsunami = new Tsunami();
+		naturalDisasters.add(tsunami);
+		volcanicEruption = new VolcanicEruption();
+		naturalDisasters.add(volcanicEruption);
 	}
 	
 	public ArrayList<NaturalDisasters> isHappening(){
+		happen();
 		return enableDisasters;
+	}
+	
+	private void happen() {
+		for(NaturalDisasters nd : naturalDisasters) {
+			nd.play();
+			if(nd.getAffectedCells().size() > 0) {
+				enableDisasters.add(nd);
+			}
+		}
 	}
 }
