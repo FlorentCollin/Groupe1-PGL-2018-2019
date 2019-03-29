29ee37f3bb29339fe8cb6e9428970aab9898aa02
lo <florent.collin@student.umons.ac.be>
2019-03-29 14:11:07 +0100

    Remove all compiled files

--- core/bin/test/worlds/hexSelected.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="hexSelected" tilewidth="256" tileheight="222" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="256" height="222" source="hex_selected.png"/>
- </tile>
-</tileset>
